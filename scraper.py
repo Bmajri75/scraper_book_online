18d88a502741d27cf29194037c5376fb1a46dd2a
Bechir Majris <bmajri@gmail.com>
2022-12-01 15:51:24 +0100

    first commit

---
+++ scraper.py
@@ -0,0 +1,5 @@
+# import du package requests, celui ci me permet de faire des requetes HTTP 
+import requests
+
+# import de Bautiful soup alias bs4 pour recuperer les donner des fichier html ou XML
+import bs4
