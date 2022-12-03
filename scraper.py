21f629977157750d3e83f59e911710ba4a761a09
Bechir Majris <bmajri@gmail.com>
2022-12-03 21:10:01 +0100

    recupere les infos et cree un fichier csv

--- scraper.py
+++ scraper.py
@@ -1,32 +1,56 @@
 # import du package requests, celui ci me permet de faire des requetes HTTP
 import requests
-
-# import de Bautifulsoup de bs4 pour recuperer les donner des fichier html ou XML
+import csv
+#  import de Bautifulsoup de bs4 pour recuperer les donner des fichier html ou XML
 from bs4 import BeautifulSoup
 
-# methode qui recupere le contenue d'une URL
+# ici je recupere l'URL principal du site
+url = "http://books.toscrape.com/index.html"
 
 
+# Je crée une fonction qui me permet de recuperer la page HTML d'un lien
 def get_request_url(url):
     result = requests.get(url)
-    return result.content
+    if result.ok:  # si le resultat est ok je retourne le contenue
+        return result.content
 
 
-# Je fais appel a ma methode et je la parse dans la variable soup
-url = "http://books.toscrape.com/catalogue/in-her-wake_980/index.html"
-soup = BeautifulSoup(get_request_url(url), 'html.parser')
+# cette fonction permet de recupere tous les lien des categories
+def get_all_category_link(url):
+    all_link = []  # j'initialise une liste vide pour pouvoir stocké les liens
+    soup = BeautifulSoup(get_request_url(url), 'html.parser')
+
+# je boucle pour chaque lien  je les ajoute avec append
+    for link in soup.find_all('a', href=True):
+        all_link.append(link['href'])
+
+    print(all_link)
 
 
-def get_text_from_page():
+get_all_category_link(url)
+
+
+def get_text_from_page(url):
+    # get_text_from_page fait appel a la fonction get_request_url
+    soup = BeautifulSoup(get_request_url(url), 'html.parser')
     # recupere la class price_color qui contien les prix des article
     informations = soup.find_all("tr")
     result = {}
     # je boucle pour cree un dictionaire des valeurs a recuper
     for information in informations:
-        result.update({information.th.get_text(): information.td.get_text()})
-
-# j'affiche le resultat
-    print(result)
+        result[information.th.get_text()] = information.td.get_text()
+    return result
 
 
-get_text_from_page()
+def create_file():
+    # Je fais appel a ma methode
+    url = "http://books.toscrape.com/catalogue/in-her-wake_980/index.html"
+    details_of_my_book = get_text_from_page(url)
+
+    en_tete = [details_of_my_book.keys()]
+    corp_text = [details_of_my_book.values()]
+    with open('data.csv', 'w') as fichier_csv:
+        file_writer = csv.writer(fichier_csv)
+        file_writer.writerows(en_tete)
+        for en_tete in (en_tete):
+            file_writer.writerows(corp_text)
