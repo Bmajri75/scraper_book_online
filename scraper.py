26f99990317fec22efec40a9b94e91a9eca8e92a
Bechir Majris <bmajri@gmail.com>
2022-12-13 16:38:21 +0100

    Mise a jour du code, mise en place de la fonction main

--- scraper.py
+++ scraper.py
@@ -77,8 +77,7 @@ def get_informations_from_page(url_book):
         informations.append(td.text)
     title = soup.find('h1').text
     informations[1] = title
-    if (soup.select(".product_page > p")[0].text):
-        description = soup.select(".product_page > p")[0].text
+    description = soup.select(".product_page > p")[0].text
     informations.insert(6, description)
     categorie = soup.select('.breadcrumb > li')[2].text
     informations.insert(7, categorie)
@@ -97,12 +96,21 @@ def create_file(url_book):
     en_tete = ("Product_page_url", "upc", "Title", "Price_including_tax", "Price_excluding_tax",
                "Pumber_available", "Product_description", "Category", "Review_rating", "Image_url")
 
-    with open('data.csv', 'a') as fichier_csv:
+    with open('data.csv', 'a', newline="", encoding="utf-8") as fichier_csv:
         file_writer = csv.writer(fichier_csv)
         file_writer.writerow(en_tete)
         file_writer.writerow(details_of_my_book)
 
 
-# # principal_function(url)
-# # A FAIRE
-# # 1 - REGLER LES LIENS DANS LA FONCTION GET CATEGORY LINK
+def main(url):
+    #! JE RECUPERE TOUTES LES CATEGORIES
+    all_categorys = get_all_categorys_link(url)
+#! START BOUCLE
+    for link in all_categorys:
+        all_books = get_books_link(link)
+        for book in all_books:
+            create_file(book)
+#! END BOUCLE
+    # # principal_function(url)
+    # # A FAIRE
+    # # 1 - REGLER LES LIENS DANS LA FONCTION GET CATEGORY LINK
